5fdfa1317881ad1e096ee36291de26628986ab52
Nattida Wong <nattidac.wong@gmail.com>
2020-09-21 03:50:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].collect do |holidays, supplies|
+    
+## holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
