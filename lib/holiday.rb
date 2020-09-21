95dd4cf099614a153405bd4a1d00a8249ca19216
Nattida Wong <nattidac.wong@gmail.com>
2020-09-21 04:08:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}"
     holiday.each do |day, item|
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
