373c15da95266fc59fe1600020cd149ba8b8279b
Nattida Wong <nattidac.wong@gmail.com>
2020-09-21 03:41:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,13 +41,18 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   holiday_hash[:spring][:memorial_day].push(supply)
-  ## or use shovel method like above
+## or use shovel method like above
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+## add a key to a hash: hash = {key: "apple"} / hash[:key5] = "lenny" 
 
+## adds key of new holidy to season
+## adds supplies for new holiday
+  holiday_hash[:season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
